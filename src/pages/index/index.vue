a71b2164636adf99c081ae18d2dadfe9efcab94d
CNBlackJ <yes.heng@icloud.com>
2018-05-03 12:19:01 +0800

    feature: todlist version1

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -19,6 +19,7 @@
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
     </form>
     <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+    <a href="/pages/todo/main" class="counter">TODO_LIST</a>
   </div>
 </template>
 
---
+++ src/pages/todo/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>TODO LIST</h1>
+    <input type="text" placeholder="enter your todo" v-model="newTodo">
+    <button @click="addTodo">done</button>
+    <ul>
+      <li v-for="todo in todoList" :key="todo.id">{{ todo.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default{
+    data () {
+      return {
+        newTodo: '',
+        todoList: [
+        { id: 1, title: 'play football', status: 1 }
+        ]
+      }
+    },
+    methods: {
+      addTodo: function () {
+        const todo = { id: this.todoList.length, title: this.newTodo, status: 1 }
+        this.todoList.push(todo)
+      }
+    }
+  }
+</script>
